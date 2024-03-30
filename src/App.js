e72ca06eb2967fbbd546f0324ddab4bbb53df1b0
Manoj.A <manojamir2911@gmail.com>
2024-03-30 10:30:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TicTacToe from "./Components/TicTacToe/TicTacToe";
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+};
+
+export default App;
